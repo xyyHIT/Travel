e8bfccf5006cf573713be3eb955d3ad6dda8bdde
xuyang <xyy.hit@126.com>
2018-06-19 18:49:54 +0800

    7-5 commit

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,51 +1,115 @@
 <template>
   <div class="icons">
-      <div class="icon">
-          <div class="icon-img">
-            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-          </div>
-          <p class="icon-desc">热门景点</p>
-      </div>
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+            <div class="icon-img">
+                <img class="icon-img-content" :src="item.imgUrl">
+            </div>
+            <p class="icon-desc">{{item.desc}}</p>
+        </div>
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      iconList: [{
+        id: '1',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票1'
+      }, {
+        id: '2',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票2'
+      }, {
+        id: '3',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票3'
+      }, {
+        id: '4',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票4'
+      }, {
+        id: '5',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票5'
+      }, {
+        id: '6',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票6'
+      }, {
+        id: '7',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票7'
+      }, {
+        id: '8',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票8'
+      }, {
+        id: '9',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票9'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
-  .icons
-      overflow hidden
-      height 0
-      padding-bottom 50%
-      .icon
-          position relative
-          float left
-          width 25%
-          height 0
-          padding-bottom 25%
-          .icon-img
-              position absolute
-              top 0
-              left 0
-              right 0
-              bottom .44rem
-              box-sizing border-box
-              padding .1rem
-              .icon-img-content
-                  display block
-                  margin 0 auto
-                  height 100%
-          .icon-desc
-              position absolute
-              left 0
-              right 0
-              bottom 0
-              height .44rem
-              line-height .44rem
-              text-align center
-              color $darkTextColor
+  @import '~styles/mixins.styl'
+    .icons >>> .swiper-container
+        height 0
+        padding-bottom 50%
+    .icon
+        position relative
+        float left
+        width 25%
+        height 0
+        padding-bottom 25%
+        .icon-img
+            position absolute
+            top 0
+            left 0
+            right 0
+            bottom .44rem
+            box-sizing border-box
+            padding .1rem
+            .icon-img-content
+                display block
+                margin 0 auto
+                height 100%
+        .icon-desc
+            position absolute
+            left 0
+            right 0
+            bottom 0
+            height .44rem
+            line-height .44rem
+            text-align center
+            color $darkTextColor
+            ellipsis()
 </style>
